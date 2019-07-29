8b5323274cfe1ec0c337955fe8b4bfd98408391f
Ilya <arzaevilya@gmail.com>
2019-07-29 12:41:25 +0700

    first

---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+
+    private JTextArea textArea;
+    private JButton btn;
+
+    public MainFrame() {
+        super("Hello World");
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Click Me!");
+
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Hello");
+            }
+        });
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}

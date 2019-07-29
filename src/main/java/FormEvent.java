10a29446bb47c12bd487a5bfad30d21c847b2fb9
Ilya <arzaevilya@gmail.com>
2019-07-29 21:16:36 +0700

    Upgrade Jlist

--- src/main/java/FormEvent.java
+++ src/main/java/FormEvent.java
@@ -4,12 +4,14 @@ public class FormEvent extends EventObject {
 
     private String name;
     private String occupation;
+    private int ageCategory;
 
-    public FormEvent(Object source, String name, String occupation) {
+    public FormEvent(Object source, String name, String occupation, int ageCat) {
         super(source);
 
         this.name = name;
         this.occupation = occupation;
+        this.ageCategory = ageCat;
     }
 
     public String getName() {
@@ -26,5 +28,9 @@ public class FormEvent extends EventObject {
 
     public void setOccupation(String occupation) {
         this.occupation = occupation;
+    }
+
+    public int getAgeCategory() {
+        return ageCategory;
     }
 }

9ff2ff1562ee2eb8523880a0c8e2d348e94aeb6f
Ilya <arzaevilya@gmail.com>
2019-07-30 22:37:19 +0700

    hidden form panel

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -64,12 +64,22 @@ public class MainFrame extends JFrame {
 
         JMenu windowMenu = new JMenu("Window");
         JMenu showMenu = new JMenu("Show");
-        JMenuItem showFormItem = new JMenuItem("Person Form");
+
+        final JCheckBoxMenuItem showFormItem = new JCheckBoxMenuItem("Person Form");
+        showFormItem.setSelected(true);
+
         showMenu.add(showFormItem);
         windowMenu.add(showMenu);
 
         menuBar.add(fileMenu);
         menuBar.add(windowMenu);
+
+        showFormItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem)e.getSource();
+                formPanel.setVisible(menuItem.isSelected());
+            }
+        });
 
         return menuBar;
     }
